425c7105464fcaf0632ea87f6fad822c79a327ce
farhanyp <36284284+farhanyp@users.noreply.github.com>
2024-03-02 04:19:23 +0700

    learn type data

---
+++ belajar-golang-dasar/type_data/go.mod
@@ -0,0 +1,3 @@
+module typedata
+
+go 1.22.0
---
+++ belajar-golang-dasar/type_data/typedata.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt";
+
+func main(){
+
+	// number
+	fmt.Println("ini adalah int", 1)
+	fmt.Println("ini juga int", 10)
+
+	// floating point
+	fmt.Println("ini floating point", 1.5)
+
+	// boolean
+	fmt.Println("ini boolean", true)
+	fmt.Println("ini boolean", false)
+
+	// string
+	fmt.Println("Farhan Yudha Pratama")
+	fmt.Println(len("Farhan Yudha Pratama"))
+	fmt.Println("Farhan Yudha Pratama"[0]) // jika begini balikannya adalah byte
+	fmt.Println("Farhan Yudha Pratama"[1]) // jika begini balikannya adalah byte
+}
