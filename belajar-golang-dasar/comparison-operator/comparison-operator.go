06b445190fd3e865ee3b163a39aa6ef57dc3e677
farhanyp <36284284+farhanyp@users.noreply.github.com>
2024-03-03 04:49:39 +0700

    Comparison Operator

---
+++ belajar-golang-dasar/comparison-operator/comparison-operator.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Math Operation
+	var a int = 10
+	var b int = 2
+	var check1 bool
+	var check2 bool
+
+	check1 = a != b
+	check2 = a > b
+
+	fmt.Println(check1)
+	fmt.Println(check2)
+}
---
+++ belajar-golang-dasar/comparison-operator/go.mod
@@ -0,0 +1,3 @@
+module comparisonoperator
+
+go 1.22.0
