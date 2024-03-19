4ddacd6188f14e1d32d1fb36018cd70d3f0942d3
farhanyp <36284284+farhanyp@users.noreply.github.com>
2024-03-20 04:38:53 +0700

    context with value

---
+++ belajar-context/context-with-value/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T){
+	contextA := context.Background()
+
+	// Create context with value
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+
+	// Get context with value
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+
+	fmt.Println(contextA.Value("b"))
+}
