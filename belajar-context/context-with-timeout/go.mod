551b8f2f26674e3f14f9e3a392be2dcc2477f457
farhanyp <36284284+farhanyp@users.noreply.github.com>
2024-03-22 19:32:02 +0700

    context with timeout

---
+++ belajar-context/context-with-timeout/go.mod
@@ -0,0 +1,3 @@
+module context-with-timeout
+
+go 1.22.0
---
+++ belajar-context/context-with-timeout/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+
+)
+
+func CreateCounter(ctx context.Context)chan int{
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select{
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContext(t *testing.T){
+	fmt.Println("Goroutine:", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5 * time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Goroutine:", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Goroutine:", runtime.NumGoroutine())
+}
