6132cbac2aa064a43265c51bba0ee59266a8dacc
farhanyp <36284284+farhanyp@users.noreply.github.com>
2024-05-06 12:51:51 +0700

    Add auth

--- belajar-golang-restful-api/main.go
+++ belajar-golang-restful-api/main.go
@@ -4,6 +4,7 @@ import (
 	"belajar-golang-restful-api/app"
 	"belajar-golang-restful-api/controller"
 	"belajar-golang-restful-api/exception"
+	"belajar-golang-restful-api/middleware"
 	"belajar-golang-restful-api/repository"
 	"fmt"
 	"net/http"
@@ -34,7 +35,7 @@ func main() {
 
 	server := http.Server{
 		Addr: "localhost:3000",
-		Handler: router,
+		Handler: middleware.NewAuthMiddleware(router),
 	}
 
 	err := server.ListenAndServe()
