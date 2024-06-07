077e3dce58525a1290c85a4ab789010dd010685f
roronoa52 <farhan.yudha2016we@gmail.com>
2024-06-07 22:56:05 +0700

    type parameter inheritance

---
+++ belajar-generic/type-parameter-inheritance/main_test.go
@@ -0,0 +1,56 @@
+package typeparameterinheritance
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Employee interface{
+	GetName() string
+}
+
+func GetName[T Employee](parameter T) string{
+	return parameter.GetName()
+}
+
+type Manager interface{
+	GetName() string
+	GetManagerName() string
+}
+
+type MyManager struct{
+	Name string
+}
+
+func(m *MyManager)GetName() string{
+	return m.Name
+}
+
+func(m *MyManager)GetManagerName() string{
+	return m.Name
+}
+
+type VicePresident interface{
+	GetName() string
+	GetVicePresidentName() string
+}
+
+type MyVicePresident struct{
+	Name string
+}
+
+func(m *MyVicePresident)GetName() string{
+	return m.Name
+}
+
+func(m *MyVicePresident)GetVicePresidentName() string{
+	return m.Name
+}
+
+func TestSample(t *testing.T){
+
+	assert.Equal(t, "Yp", GetName[Manager](&MyManager{Name: "Yp"}))
+	assert.Equal(t, "Yp", GetName[VicePresident](&MyVicePresident{Name: "Yp"}))
+
+}
