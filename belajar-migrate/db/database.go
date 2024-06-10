3ccf389f9838320f867fa253851045a22524b30e
roronoa52 <farhan.yudha2016we@gmail.com>
2024-06-10 13:00:52 +0700

    belajar migrate

---
+++ belajar-migrate/db/database.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+
+	db, err := sql.Open("postgres", "postgres://postgres:12345678@localhost/belajar_golang_migration_pzn?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+
+	// Untuk migrate
+	// migrate create -ext sql -dir db/migrations create_table_category
+
+	// Untuk migrate up
+	// migrate -database "postgres://postgres:12345678@localhost/belajar_golang_migration_pzn?sslmode=disable" -path db/migrations up
+
+	// Untuk migrate down
+	// migrate -database "postgres://postgres:12345678@localhost/belajar_golang_migration_pzn?sslmode=disable" -path db/migrations down
+}
