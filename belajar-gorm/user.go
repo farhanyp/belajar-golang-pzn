2df0eaf2eb8566237fc62790e3a3d5bbc1bb9e5d
roronoa52 <farhan.yudha2016we@gmail.com>
2024-06-11 10:50:55 +0700

    embedded model column

--- belajar-gorm/user.go
+++ belajar-gorm/user.go
@@ -2,10 +2,18 @@ package main
 
 import "time"
 
+type Name struct{
+
+	FirstName	string	`gorm:"column:first_name"`
+	MiddleName	string	`gorm:"column:middle_name"`
+	lastName	string	`gorm:"column:last_name"`
+
+}
+
 type User struct {
 	ID        string 	`gorm:"primary_key;column:id"`
 	Password  string 	`gorm:"column:password"`
-	Name      string 	`gorm:"column:name"`
+	Name      Name 		`gorm:"embedded"`
 	CreatedAt time.Time	`gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt time.Time	`gorm:"column:update_at;autoCreateTime;autoUpdateTime"`
 }
